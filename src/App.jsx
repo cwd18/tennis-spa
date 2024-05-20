97bc8d2b3228b39036f1508e94c52e0a0e935a79
Charles Davies <charles.davies18@gmail.com>
2024-05-20 08:46:07 +0100

    Set apiServer to GAE if not on localhost

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import 'purecss/build/pure.css';
 import './App.css';
+import { useEffect } from "react";
 import { Route, Routes } from 'react-router-dom';
 import Start from './Start';
 import AdminView from './AdminView';
 import UserView from './UserView';
 
 function App() {
-
+  useEffect(() => {
+    if (!window.location.hostname.includes('localhost')) {
+      // If not localhost, assume we are running on Google Cloud
+      globalData.apiServer = 'https://direct-terminal-412715.nw.r.appspot.com/';
+    }
+  }, []);
   return (
     <div>
       <Routes>
-        <Route path="/start/:token" element={<Start />} />
+        <Route path="/newstart/:token" element={<Start />} />
         <Route path="/admin" element={<AdminView />} />
         <Route path="/user/:seriesid/:userid" element={<UserView />} />
       </Routes>
