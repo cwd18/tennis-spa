c380f758faf2fa34b4822918e629b051d8ee26fd
Charles Davies <charles.davies18@gmail.com>
2024-06-14 14:51:10 +0100

    Fix fixture index reset after input

--- src/FixtureView.jsx
+++ src/FixtureView.jsx
@@ -9,7 +9,7 @@ import SetPlaying from "./SetPlaying";
 
 function FixtureView({ seriesid, userid, setUserid }) {
   const [fixtures, setFixtures] = useState([]);
-  const [fixtureIndex, setFixtureIndex] = useState();
+  const [fixtureIndex, setFixtureIndex] = useState(-1);
   const [viewTime, setViewTime] = useState(0);
   const { apiServer, role } = globalData;
   const [error, setError] = useState(null);
@@ -24,12 +24,14 @@ function FixtureView({ seriesid, userid, setUserid }) {
       })
       .then((response) => {
         setFixtures(response);
-        const daysToNextFixture =
-          (new Date(response[0].FixtureDate) - new Date()) /
-          (1000 * 60 * 60 * 24);
-        // show the next fixture if it's more than 1 day away, otherwise show the latest fixture
-        const index = daysToNextFixture > 1 ? 0 : 1;
-        setFixtureIndex(index);
+        if (fixtureIndex === -1) {
+          const daysToNextFixture =
+            (new Date(response[0].FixtureDate) - new Date()) /
+            (1000 * 60 * 60 * 24);
+          // show the next fixture if it's more than 1 day away, otherwise show the latest fixture
+          const index = daysToNextFixture > 1 ? 0 : 1;
+          setFixtureIndex(index);
+        }
       })
       .catch((error) => {
         console.error(error.toString());
