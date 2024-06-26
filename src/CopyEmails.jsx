e055db312d907027753d0d8fd07a999ff0e58028
Charles Davies <charles.davies18@gmail.com>
2024-06-26 11:33:40 +0100

    Show status in copy emails to clipboard

--- src/CopyEmails.jsx
+++ src/CopyEmails.jsx
@@ -1,20 +1,27 @@
 import globalData from "./GlobalData";
+import { useState } from "react";
 
 function CopyEmails({ fixtureid }) {
   const { apiServer } = globalData;
+  const [copying, setCopying] = useState(false);
 
   const copyEmailsToClipboard = () => {
+    setCopying(true);
     fetch(apiServer + "/api/getEmailList/" + fixtureid, {
       credentials: "include",
     })
       .then((response) => response.text())
-      .then((response) => navigator.clipboard.writeText(response));
+      .then((response) => {
+        navigator.clipboard.writeText(response);
+        setCopying(false);
+      });
   };
+
   return (
     <div>
       <br />
-      <button class="pure-button" onClick={copyEmailsToClipboard}>
-        Copy emails to clipboard
+      <button className="pure-button" onClick={copyEmailsToClipboard}>
+        {copying ? "Copying..." : "Copy emails to clipboard"}
       </button>
     </div>
   );
