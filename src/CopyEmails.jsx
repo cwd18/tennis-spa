dadbd750be6ee9824b9bd2f4c77eb00a61d78cf7
Charles Davies <charles.davies18@gmail.com>
2024-07-07 10:19:08 +0100

    Improve copy emails to clipboard

--- src/CopyEmails.jsx
+++ src/CopyEmails.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 function CopyEmails({ fixtureid }) {
   const { apiServer } = globalData;
   const [copying, setCopying] = useState(false);
-  const [copied, setCopied] = useState(false); // Added state for copied message
+  const [copied, setCopied] = useState(0);
 
   const copyEmailsToClipboard = () => {
     setCopying(true);
@@ -13,10 +13,18 @@ function CopyEmails({ fixtureid }) {
     })
       .then((response) => response.text())
       .then((response) => {
-        navigator.clipboard.writeText(response);
-        setCopying(false);
-        setCopied(true); // Set copied state to true after successful copy
-        setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+        navigator.clipboard.writeText(response).then(
+          () => {
+            setCopying(false);
+            setCopied(1); // Set copied state to 1 after successful copy
+            setTimeout(() => setCopied(0), 2000); // Reset copied state after 2 seconds
+          },
+          () => {
+            setCopying(false);
+            setCopied(2); // Set copied state to 2 after unsuccessful copy
+            setTimeout(() => setCopied(0), 2000); // Reset copied state after 2 seconds
+          }
+        );
       });
   };
 
@@ -26,8 +34,8 @@ function CopyEmails({ fixtureid }) {
       <button className="pure-button" onClick={copyEmailsToClipboard}>
         {copying ? "Copying..." : "Copy emails to clipboard"}
       </button>
-      {copied && <p>Emails copied to clipboard</p>}{" "}
-      {/* Display copied message if copied state is true */}
+      {copied == 1 && <p>Emails copied to clipboard</p>}
+      {copied == 2 && <p>Unable to copy emails to clipboard</p>}
     </div>
   );
 }
